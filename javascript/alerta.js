cb951283bceb41b6de47a7f3f1b432f063bfd957
Arthur Gabriel da Silva <arthurgsilva0201@gmail.com>
2024-06-24 21:42:57 -0300

    paginas diagnostico

---
+++ javascript/alerta.js
@@ -0,0 +1,17 @@
+const botao = document.querySelector(".btnEnviar");
+const email = document.querySelector(".email");
+const comentario = document.querySelector(".comentario");
+
+botao.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if(email.value == "" && comentario.value == ""){
+        alert("Preencha os campos antes de enviar o formulário!");
+    }
+    else{
+        alert("Sua sugestão foi enviada! Agradecemos pelo feedback e em breve entraremos em contato!");
+    }
+
+    email.value = "";
+    comentario.value = "";
+})
+
