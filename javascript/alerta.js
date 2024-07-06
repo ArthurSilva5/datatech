19e9113615f81d934a06a0c591dae61b012f62cb
Arthur Gabriel da Silva <arthurgsilva0201@gmail.com>
2024-07-05 22:43:35 -0300

    atualizacao paginas

--- javascript/alerta.js
+++ javascript/alerta.js
@@ -4,7 +4,7 @@ const comentario = document.querySelector(".comentario");
 
 botao.addEventListener("click", (evento) => {
     evento.preventDefault();
-    if(email.value == "" && comentario.value == ""){
+    if(email.value == "" || comentario.value == ""){
         alert("Preencha os campos antes de enviar o formulário!");
     }
     else{
